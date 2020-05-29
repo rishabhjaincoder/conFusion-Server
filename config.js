d3a03495945ea5c9b2076d15515b052785fee824
Rishabh jain <rishabhjain.rdps@gmail.com>
2020-05-30 00:22:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
     'secretKey':'12345-67890-09876-54321',
     'mongoUrl':'mongodb://localhost:27017/conFusion',
     'facebook': {
-        clientId: '893247471176536',
-        clientSecret: 'c911d0767d08a729311dee995ed87194' 
+        clientId: '',
+        clientSecret: '' 
     }
 }
